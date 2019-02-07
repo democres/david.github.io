1184c3f5102ccdd5d1bdda847af6520799a91d28
Democres <SoftwareSolutionsBA@gmail.com>
2019-02-07 10:24:21 -0500

    Add page header

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+  var typed = new Typed('#typed',{
+    stringsElement: '#typed-strings',
+    startDelay: 1100,
+    backSpeed: 35,
+    typeSpeed: 45,
+    loop: true,
+    smartBackspace: true,
+    backDelay: 1100
+  });
